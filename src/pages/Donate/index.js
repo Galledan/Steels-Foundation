e3cea6befdf8a928d5ccebb6a6aa306a4acd53dd
Galledan <onrclklr@outlook.com>
2022-08-23 14:44:25 +0300

    new changes

---
+++ src/pages/Donate/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavigationBar from '../../components/NavigationBar'
+
+function Donate() {
+  return (
+    <div>
+         <NavigationBar />
+
+         <h1>this is donate page</h1>
+    </div>
+  )
+}
+
+export default Donate
---
+++ src/pages/Join/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavigationBar from '../../components/NavigationBar'
+
+function Join() {
+  return (
+    <div>
+        <NavigationBar />
+
+        <h1>This is join page</h1>
+    </div>
+  )
+}
+
+export default Join
