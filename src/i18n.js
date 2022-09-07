3ac714ca33eb09a9b5ebaa767312bc086d68e7df
Galledan <onrclklr@outlook.com>
2022-09-07 11:41:36 +0300

    changes

---
+++ src/i18n.js
@@ -0,0 +1,82 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+// the translations
+// (tip move them in a JSON file and import them,
+// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
+const resources = {
+  en: {
+  },
+  tr: {
+    translation: {
+      "Support A Child's Education": "Bir çocuğun geleceğini destekle",
+      "Quality Education is every child's right.": "Nitelikli eğitim her çocuğun hakkıdır.",
+      "Support a Child": "Destekle",
+      "Join Us to Help": "Yardım etmek için bize katıl",
+      "Volunteer and help children to get them quality education.":" Gönüllü olarak bize katılın ve çocuklara nitelikli eğitim alması için yardım edin.",
+      "Join Us":"Bize Katıl",
+      "Why Quality Education is Important?":"Nitelikli Eğitim Neden Önemli?",
+      "Education is the key for succeding in many Sustainable Development Goals(SDGs). People can walk away from poverty when they got quality education. That's why Education helps decreasing inequality and ensure social gender equality. Also helps to have a healthy and sustainable life wherever people are. Education also helps to improve clemency and build a peaceful society.":"Eğitim bir çok Sürdürülebilir Kalkınma Hedefinin anahtarıdır. İnsanlar nitelikli eğitim alabildiklerinde yoksulluk girdabından da kurtulabilirler. Bu nedenle eğitim eşitsizliklerinin azaltılması ve toplumsal cinsiyet eşitliğin sağlanmasına yardımcı olur. Ayrıca nerede olursa olsun insanların daha sağlıklı ve sürdürülebilir yaşamları olmasını sağlar. Eğitim aryıca hoşgörünün gelişmesi ve barışcıl toplumların inşasına da destek olur.",
+      "Thanks to your donations we provide stationary items, medicalsupport, uniforms, nutritious food and etc. to the children who cannot afford them":"Bağışlarınız sayesinde çocukların kırtasiye eşyaları, sağlık hizmetleri, üniformalar, besleyici gıdalar vb. ihtiyaçları karşılanıyor.",
+      "Donate":"Bağış Yap",
+      "Volunteer for children and be a part of our non-profit foundation. Help us to give children a quality education which is their right.":"Gönüllü olarak kâr gütmeyen vakfımıza katılın ve çocuklara nitelikli eğitim vermemize yardımcı olun.",
+      "Our partners":"Partnerlerimiz",
+      "Latest Donations":"Son Yapılan Bağışlar",
+      "donated":"bağış yaptı",
+
+
+      "You are about to change a child's life!":"Bir çocuğun hayatını değiştirmek üzeresin!",
+      "Amount of money":"Bağış miktarı",
+      "Your Info":"Bilgileriniz",
+      "Name":"Adınız",
+      "Your name":"Adınızı yazınız",
+      "Surname":"Soyadınız",
+      "Your surname":"Soyadınızı yazınız",
+      "Email address":"Email adresiniz",
+      "Enter email":"Email adresinizi giriniz",
+      "Phone Number":"Telefon numaranız",
+      "Phone number here":"Telefon numaranızı giriniz",
+      "Address":"Adresiniz",
+      "Your address here":"Adresinizi giriniz",
+      "Please select a donate type":"Bağış tipini seçiniz",
+      "Go to Payment":"Ödemeye geçin",
+
+      "Card Number":"Kart Numarası",
+      "Full Name":"Kart Üzerindeki İsim",
+      "MM/YY":"AA/YY",
+      "Thanks for your donation!":"Bağışınız için teşekkürler!",
+      "You can be sure this money will be used to change a life of a child.":"Bu paranın bir çocuğun hayatının değişmesi için kullanılacağından şüpheniz olmasın.",
+      "Pay":"Öde",
+
+      "Date of Birth":"Doğum tarihiniz",
+      "Why do you wanna be part of us?":"Neden bize katılmak istiyorsunuz?",
+      "Explain your reasons":"Sebebiniz",
+      "Gender":"Cinsiyet",
+      "Select Gender":"Cinsiyetinizi seçiniz",
+      "Male":"Erkek",
+      "Female":"Kadın",
+      "Prefer not to say":"Söylemek istemiyorum",
+      "Thanks for deciding to join us! Help us to grow our team and lets provide quality education to every children we can reach.":"Bize katılmak istediğin için teşekkür ederiz! Büyümemize yardımcı ol ve ulaşabildiğimiz her çocuğa nitelikli bir eğitim kazandıralım.",
+      "Register":"Başvur",
+      "Registration":"Başvuru Formu",
+      "TR":"ENG"
+
+
+    }
+  }
+};
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources,
+    lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
+    // if you're using a language detector, do not define the lng option
+
+    interpolation: {
+      escapeValue: false // react already safes from xss
+    }
+  });
+
+  export default i18n;
--- src/pages/Payment/index.js
+++ src/pages/Payment/index.js
@@ -5,6 +5,8 @@ import "./payment.css";
 import Button from "react-bootstrap/Button";
 import NavigationBar from "../../components/NavigationBar";
 import emailjs from '@emailjs/browser';
+import Modal from "react-bootstrap/Modal"
+import {useTranslation} from 'react-i18next';
 
 function Payment() {
   const [number, setNumber] = useState("");
@@ -20,6 +22,12 @@ function Payment() {
   const firstRender = useRef(true)
 
   const payment = useRef()
+
+  const { t} = useTranslation();
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
     
    
   useEffect(() => {
@@ -34,15 +42,24 @@ function Payment() {
     }
   
   }, [expiry]) 
-   
+
+  const validatePayment = () => {
+    if (!name || name === '') return false
+    if (!number || number === '') return false
+    if (!expiry || expiry === '') return false
+    if (!cvc || cvc === '') return false
+    else return true
+    
+  }
 
   useEffect(() => {
     ref.current.focus();
   }, []);
 
+
   const onSubmit = () => {
-  
     emailjs.sendForm('service_di4caar', 'template_s7wrlnf', payment.current, 'vfIYz4eqhWMQv2r60')
+    handleShow()
 
   }
  
@@ -65,7 +82,7 @@ function Payment() {
             type="tel"
             name="payment.number"
             maxLength={16}
-            placeholder="Card Number"
+            placeholder={t("Card Number")}
             value={number}
             onChange={(e) => setNumber(e.target.value)}
             onFocus={(e) => setFocus(e.target.name)}
@@ -74,7 +91,7 @@ function Payment() {
           <input
             type="text"
             name="payment.name"
-            placeholder="Full Name"
+            placeholder={t("Full Name")}
             value={name}
             onChange={(e) => setName(e.target.value)}
             onFocus={(e) => setFocus(e.target.name)}
@@ -83,7 +100,7 @@ function Payment() {
             type="tel"
             name="payment.expiry"
             maxLength={5}
-            placeholder="MM/YY"
+            placeholder={t("MM/YY")}
             value={expiry}
             onChange={(e) => setExpiry(e.target.value)}
             onFocus={(e) => setFocus(e.target.name)}
@@ -98,12 +115,18 @@ function Payment() {
             onFocus={(e) => setFocus(e.target.name)}
           />
         </form>
-        <Button onClick={onSubmit} variant="primary" type="submit">Pay</Button>
+        <Button onClick={onSubmit} variant="primary" type="submit" disabled={!validatePayment()}>{t("Pay")}</Button>
+        <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Thanks for your donation</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Please do not forget the share this foundation with your friends! <a href="localhost:3000">Steels Foundation</a></Modal.Body>
+      </Modal>
       </div>
       <div className="text-container">
-        <h1>Thanks for your donation!</h1>
+        <h1>{t("Thanks for your donation!")}</h1>
         <p>
-            You can be sure this money will be used to change a life of a child.
+        {t("You can be sure this money will be used to change a life of a child.")}
         </p>
       </div>
     </div>
