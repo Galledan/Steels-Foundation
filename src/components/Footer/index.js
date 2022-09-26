027dc492a3a51ec258c31f2925ffcaf8cef8dad2
Galledan <onrclklr@outlook.com>
2022-09-26 13:14:52 +0300

    feedback changes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,17 +9,17 @@ function Footer() {
             <div className='row'>
                 <div className='col'>
                     <ul className='list-unstyled'>
-                        <li><i href="https://twitter.com/galledann"class="fab fa-twitter fa"></i></li>
-                        <li><i href="https://www.instagram.com/onrclklrr"class="fab fa-instagram fa"></i></li>
-                        <li><i href="https://www.linkedin.com/in/onur-çelikler-429b02187/" class="fab fa-linkedin fa"></i></li>
+                        <li><a href="https://twitter.com/galledann"><i class="fab fa-twitter fa"></i></a></li>
+                        <li><a href="https://www.instagram.com/onrclklrr"><i class="fab fa-instagram fa"></i></a></li>
+                        <li><a href="https://www.linkedin.com/in/onur-çelikler-429b02187/" ><i class="fab fa-linkedin fa"></i></a></li>
                         <li><a href="/admin">Admin</a></li>
                     
                     </ul>
                 </div>
                 <div className='col'>
                     <ul className='list-unstyled'>
-                        <li><i class="fas fa-envelope"></i> info@steels.com</li>
-                        <li><i class="fas fa-phone"></i> +90 505 333 90 31</li>   
+                        <li><a href="mailto:onrclklr@outlook.com"><i class="fas fa-envelope"></i> onrclklr@outlook.com</a></li>
+                        <li><a href="https://api.whatsapp.com/send/?phone=905053339031&text&type=phone_number&app_absent=0"><i class="fas fa-phone"></i> +90 505 333 90 31</a></li>   
                     </ul>
              
                 
