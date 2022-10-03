46bf3fa6038924a8596b7b1573abbad5215a485c
Galledan <onrclklr@outlook.com>
2022-10-03 23:47:43 +0300

    fixed footer rel

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,17 +9,17 @@ function Footer() {
             <div className='row'>
                 <div className='col'>
                     <ul className='list-unstyled'>
-                        <li><a href="https://twitter.com/galledann"><i class="fab fa-twitter fa"></i></a></li>
-                        <li><a href="https://www.instagram.com/onrclklrr"><i class="fab fa-instagram fa"></i></a></li>
-                        <li><a href="https://www.linkedin.com/in/onur-çelikler-429b02187/" ><i class="fab fa-linkedin fa"></i></a></li>
+                        <li><a href="https://twitter.com/galledann" rel="noopener noreferrer"><i class="fab fa-twitter fa"></i></a></li>
+                        <li><a href="https://www.instagram.com/onrclklrr" rel="noopener noreferrer"><i class="fab fa-instagram fa"></i></a></li>
+                        <li><a href="https://www.linkedin.com/in/onur-çelikler-429b02187/" rel="noopener noreferrer"><i class="fab fa-linkedin fa"></i></a></li>
                         <li><a href="/admin">Admin</a></li>
                     
                     </ul>
                 </div>
                 <div className='col'>
                     <ul className='list-unstyled'>
-                        <li><a href="mailto:onrclklr@outlook.com"><i class="fas fa-envelope"></i> onrclklr@outlook.com</a></li>
-                        <li><a href="https://api.whatsapp.com/send/?phone=905053339031&text&type=phone_number&app_absent=0"><i class="fas fa-phone"></i> +90 505 333 90 31</a></li>   
+                        <li><a href="mailto:onrclklr@outlook.com" rel="noopener noreferrer"><i class="fas fa-envelope"></i> onrclklr@outlook.com</a></li>
+                        <li><a href="https://api.whatsapp.com/send/?phone=905053339031&text&type=phone_number&app_absent=0" rel="noopener noreferrer"><i class="fas fa-phone"></i> +90 505 333 90 31</a></li>   
                     </ul>
              
                 
